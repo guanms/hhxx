c36a37cd10b35a12d42bf6de5a5f142cfb010d15
guanshuai <215216569@qq.com>
2019-08-10 13:48:58 +0800

    worksoace.xml

--- movie-server/src/main/java/com/gms/movie/MovieApplication.java
+++ movie-server/src/main/java/com/gms/movie/MovieApplication.java
@@ -2,9 +2,17 @@ package com.gms.movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MovieApplication {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MovieApplication.class, args);
