d1d7927175a4e5e76c5abb89d47741c88beca5de
guanshuai <215216569@qq.com>
2019-08-30 09:39:28 +0800

    eureka加入用户认证

--- eureka-authenticating/src/main/java/com/gms/eureka/EurekaAuthenticatingApplication.java
+++ eureka-authenticating/src/main/java/com/gms/eureka/EurekaAuthenticatingApplication.java
@@ -1,12 +1,29 @@
 package com.gms.eureka;
 
+import com.netflix.discovery.DiscoveryClient;
+//import com.sun.jersey.api.client.filter.ClientFilter;
+//import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaAuthenticatingApplication {
+
+//    @Bean
+//    public DiscoveryClient.DiscoveryClientOptionalArgs discoveryClientOptionalArgs(){
+//        DiscoveryClient.DiscoveryClientOptionalArgs discoveryClientOptionalArgs = new DiscoveryClient.DiscoveryClientOptionalArgs();
+//        List<ClientFilter> filterList = new ArrayList<>();
+//        filterList.add(new HTTPBasicAuthFilter("root", "root"));
+//        discoveryClientOptionalArgs.setAdditionalFilters(filterList);
+//
+//        return discoveryClientOptionalArgs;
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaAuthenticatingApplication.class, args);
